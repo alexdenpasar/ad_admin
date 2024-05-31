1cc0602c308cf22ba31f645fb139cbf598bbf3e8
Alexander Kovrovsky <a.s.kovrovskiy@gmail.com>
2024-05-31 13:46:49 +0300

    Checking for new users or users removed from groups specified in the configuration file.

--- check_adm.py
+++ check_adm.py
@@ -1,7 +1,9 @@
 from ldap3 import Server, Connection, ALL, NTLM
 import configparser
 import time
+import json
 
+current_user_list = []
 
 def read_config():
     while True:
@@ -17,8 +19,34 @@ def read_config():
         time.sleep(1)
 
         return c_server_ad, c_user_ad, c_password_user_ad,c_search_base_ad, c_search_filter_ad, c_attributes_ad
+
+def check_users():
+
+    with open('users.json', 'r', encoding='utf-8') as f:
+        prev_user_list = json.load(f)
+        #print(prev_user_list)
+
+    only_in_prev_user_list = [item for item in prev_user_list if item not in current_user_list]
+    only_in_current_user_list = [item for item in current_user_list if item not in prev_user_list]
+
+    if len(only_in_current_user_list) > 0:
+        for user in only_in_current_user_list:  
+            username = str(user).split(",")[0].replace("CN=","")
+            print(f"User '{username}' was granted {read_config()[4]} rights.")
+
+    if len(only_in_prev_user_list) >  0:        
+        for user in only_in_prev_user_list:  
+            username = str(user).split(",")[0].replace("CN=","")
+            print(f"User '{username}' no longer has {read_config()[4]} rights.")
+
+    with open('users.json', 'w', encoding='utf-8') as f:
+        json.dump(current_user_list, f, ensure_ascii=False, indent=4)
     
-def check_adm():
+    only_in_prev_user_list.clear()
+    only_in_current_user_list.clear()
+    current_user_list.clear()
+
+def read_AD():
     server_ad, user_ad, password_user_ad, search_base_ad, search_filter_ad, attributes_ad = read_config()
 
     server = Server(server_ad, get_info=ALL)
@@ -28,12 +56,17 @@ def check_adm():
 
     members = conn.entries[0].member.values if conn.entries else []
 
-    print(f"Администраторы домена: {len(members)}")
+    #print(f"Администраторы домена: {len(members)}")
     for member in members:
-        print(member)
+        current_user_list.append(member)
+        #print(member)
 
     conn.unbind()
 
+    check_users()
+
 if __name__ == "__main__":
 
-    check_adm()
+    while True:
+        read_AD()
+        time.sleep(30)
